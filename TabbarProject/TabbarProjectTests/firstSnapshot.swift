474a4cad5fb892cf607d8d39221b021f0e72505d
Saritha Parsoya <sparsoya@ebay.com>
2019-02-08 21:42:24 -0800

    attempt

---
+++ TabbarProject/TabbarProjectTests/firstSnapshot.swift
@@ -0,0 +1,35 @@
+//
+//  firstSnapshot.swift
+//  TabbarProjectTests
+//
+//  Created by Parsoya, Saritha(AWF) on 2/1/19.
+//  Copyright © 2019 Parsoya, Saritha(AWF). All rights reserved.
+//
+
+import foundations
+import XCTest
+import FBSnapshotTestCase
+
+class firstSnapshot: FBSnapshotTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
